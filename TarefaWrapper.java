98ab113315cf4e6345ffd5a69c56c960b45c04d0
Guilherme Guimarães <102836703+GuimasGuimaraes@users.noreply.github.com>
2023-11-11 12:48:24 -0300

    first commit

---
+++ TarefaWrapper.java
@@ -0,0 +1,11 @@
+public class TarefaWrapper {
+    public static void main(String[] args) {
+        int minhaIdade = 26;
+        System.out.println("Minha idade é : " + minhaIdade);
+
+        int minhaIdade2 = Integer.valueOf(26);
+
+        System.out.println("Minha idade é : " + minhaIdade2);
+
+    }
+}
